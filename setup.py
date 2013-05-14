98f78b5f29ba7e28f8a503828a74ff770c3844dc
Ian Johnson <ian.johnson@appliedlanguage.com>
2013-05-14 10:13:36 +0100

    Bumped version number

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pypeline",
-    version = "0.2.3",
+    version = "0.2.4",
     packages = find_packages("src", exclude = ["*tests"]),
     package_dir = {'': 'src'},
 
